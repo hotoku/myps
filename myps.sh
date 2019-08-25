f918aa90ef9b19f06da51ed8b45c95f988690677
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-08-25 09:52:05 +0900

    アップデート色々
    1. パスワード変更機能追加
    2. パスワード生成機能追加
    3. パスワードコピー時に、末尾の改行記号を削除
    4. レコード追加時に、空いている番号を利用するように変更
    5. search関数で、引数のkeyがパスワードに含まれている場合のバグを修正
    6. initialize関数の削除。代わりにデータファイルがない場合のコメントをに生成コマンドを記述

--- myps.sh
+++ myps.sh
@@ -17,9 +17,11 @@ print_usage(){
     echo "-I <num>                 : print id"
     echo "-a 'key id password'     : add record (Do not forget quotation!)"
     echo "-u 'num key id password' : update record (Do not forget quotation!)"
+    echo "-c <num> <password>      : change password"
     echo "-D <num>                 : delete record"
     echo "-A                       : print all"
     echo "-s <string>              : search"
+    echo "-g                       : generate password"
 }
 
 list_all_keys(){
@@ -28,7 +30,7 @@ list_all_keys(){
 
 copy_password(){
     num=$1
-    decrypt | grep "^${num} " | cut -f4 -d" " | pbcopy
+    decrypt | grep "^${num} " | cut -f4 -d" " | tr -d "\n" | pbcopy
     decrypt | grep "^${num} " | cut -f1,2,3 -d" "
 }
 
@@ -39,7 +41,7 @@ print_password(){
 
 copy_id(){
     num=$1
-    decrypt | grep "^${num} " | cut -f3 -d" " | pbcopy
+    decrypt | grep "^${num} " | cut -f3 -d" " | tr -d "\n" | pbcopy
 }
 
 print_id(){
@@ -55,10 +57,12 @@ add_record(){
         alert_invalid_form_exit
     fi
 
-    num=$(decrypt | tail -n1 | cut -f1 -d" ")
-    num2=$((${num} + 1))
-    
-    do_add_record "${num2}" "${key}" "${id}" "${pass}"
+    num=$(min_miss)
+    if [ -z "${min_miss}" ]; then
+        num=$(num_record)
+        num=$(($num + 1))
+    fi
+    do_add_record "${num}" "${key}" "${id}" "${pass}"
 }
 
 do_add_record(){
@@ -66,7 +70,7 @@ do_add_record(){
     key=$2
     id=$3
     pass=$4
-    decrypt | cat - <(echo "${num}" "${key}" "${id}" "${pass}") | encrypt
+    decrypt | cat - <(echo "${num}" "${key}" "${id}" "${pass}") | sort -k1 -n | encrypt
 }
 
 update_record(){
@@ -89,6 +93,18 @@ do_update_record(){
     decrypt | grep -v "^${num} " | cat - <(echo ${num} ${key} ${id} ${pass}) | sort -k1 -n | encrypt
 }
 
+change_password(){
+    num=$1
+    pass=$2
+    key=`decrypt | grep "^${num} " | cut -f2 -d " "`
+    id=`decrypt | grep "^${num} " | cut -f3 -d " "`
+    echo num="${num}"
+    echo key="${key}"
+    echo id="${id}"
+    echo pass="${pass}"
+    update_record "${num}" "${key}" "${id}" "${pass}"
+}
+
 alert_invalid_form_exit(){
     echo "invalid form"
     print_usage
@@ -106,7 +122,7 @@ delete_record(){
 
 do_delete_record(){
     num=$1
-    decrypt | grep "^${num} " | pbcopy
+    decrypt | grep "^${num} " | tr -d "\n" | pbcopy
     echo "the record for '${num}' is copied in clipboard"
     decrypt | grep -v "^${num} " | encrypt
 }
@@ -114,7 +130,15 @@ do_delete_record(){
 search(){
     key=$1
     decrypt | cut -f1,2,3 -d" " | grep "${key}"
-    decrypt | grep "${key}" | cut -f4 -d" " | pbcopy
+
+    ids=($(decrypt | cut -f1,2,3 -d" " | grep "${key}" | cut -f1 -d" "))
+    if [ ${#ids[@]} = 1 ]; then
+        copy_password ${ids[0]} > /dev/null
+    fi
+}
+
+generate_password(){
+   openssl rand -base64 12 | fold -w 10 | head -1 | tr -d "\n" | pbcopy
 }
 
 decrypt(){
@@ -125,15 +149,19 @@ encrypt(){
     gpg -e -r "myps" -o ${DATAFILE}
 }
 
-initialize(){
-    echo | grep -v "^$" | encrypt
-}
-
 isnum(){
     str=$1
     expr ${str} + 1 > /dev/null 2>&1
     ret=$?
     test ${ret} -lt 2
+}
+
+min_miss(){
+    decrypt | awk 'NR!=$1{print NR; exit}'
+}
+
+num_record(){
+    decrypt | wc -l
 }
 
 if ! [ -r ~/.myps ]; then
@@ -148,23 +176,31 @@ if [ -z ${DATAFILE} ]; then
 fi
 
 if ! [ -r ${DATAFILE} ]; then
-    initialize
+    echo ${DATAFILE} does not exists.
+    cat <<EOF
+It is necessary to create data file manually. Run the next command.
+$ echo | grep -v "^$" | gpg -e -r myps -o ${DATAFILE}
+EOF
+    exit 1
 fi
 
 
-getopts lp:P:i:I:a:u:D:As:h OPT
+
+getopts lp:P:i:I:a:u:c:D:As:gh OPT
 case ${OPT} in
-    l) list_all_keys            ; exit ;;
-    p) copy_password ${OPTARG}  ; exit ;;
-    P) print_password ${OPTARG} ; exit ;;
-    i) copy_id ${OPTARG}        ; exit ;;
-    I) print_id ${OPTARG}       ; exit ;;
-    a) add_record ${OPTARG}     ; exit ;;
-    u) update_record ${OPTARG}  ; exit ;;
-    D) delete_record ${OPTARG}  ; exit ;;
-    A) decrypt                  ; exit ;;
-    s) search ${OPTARG}         ; exit ;;
-    h) print_usage              ; exit ;;
+    l) list_all_keys                   ; exit ;;
+    p) copy_password ${OPTARG}         ; exit ;;
+    P) print_password ${OPTARG}        ; exit ;;
+    i) copy_id ${OPTARG}               ; exit ;;
+    I) print_id ${OPTARG}              ; exit ;;
+    a) add_record ${OPTARG}            ; exit ;;
+    u) update_record ${OPTARG}         ; exit ;;
+    c) shift 1; change_password "$@"   ; exit ;; # -cを捨てている
+    D) delete_record ${OPTARG}         ; exit ;;
+    A) decrypt                         ; exit ;;
+    s) search ${OPTARG}                ; exit ;;
+    g) generate_password               ; exit ;;
+    h) print_usage                     ; exit ;;
 esac
 
 if [ -z "$1" ]; then
